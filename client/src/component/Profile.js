1f6e350113ec1c5b2cb78ce54f41f567c82ddb19
Ngo Thang <ngoxuanthangnyc.com>
2022-04-11 21:02:44 +0700

    profile

--- client/src/component/Profile.js
+++ client/src/component/Profile.js
@@ -1,10 +1,31 @@
+import react from "react"
+import NavBar from "./NavBar"
+import Footer from "./Footer"
+import avatar from "../image/avatar.jpg"
+import { Container } from "react-bootstrap"
 
 
 
 
 export default function Profile() {
     return (
-        <p>Profile</p>
+        <>
+        <NavBar/>
+        <div className="profile-container">
+            <div className="black-title"/>
+            <img src={avatar} />
+            <Container>
+                
+
+            </Container>
+
+        </div>
+
+
+
+        <Footer/>
+
+        </>
     )
 }
 

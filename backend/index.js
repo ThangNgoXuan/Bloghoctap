1ed8e565639db2d1714e67e018d4f3cc5161c11d
Hieu-Hieu <your-public-email>
2022-04-19 18:45:23 +0700

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import client from './connection.js';
 import { api } from './routers/index.js';
+import cors from 'cors';
 
 const app = express();
 
 // parse json request body
 app.use(express.json());
+
+app.use(cors())
 
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));

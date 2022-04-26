e088ddb89d5d9499921010c55c3be9a51e672df9
Hieu-Hieu <your-public-email>
2022-04-26 21:59:51 +0700

    add some apis

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,72 +1,116 @@
 import generateToken from '../utils/generateToken.js';
 import client from '../connection.js';
+import bcrypt from 'bcryptjs';
 
 const userIndex = 'users';
 
 const registUser = async (req, res) => {
-    const { username, email, password, name } = req.body
+    const { email, password, name } = req.body
 
-    //can kiem tra email or user ton tai hay ko
     try {
-        let err;
-        client.index({
+        const existsUser = await client.search({
             index: userIndex,
-            type: '_doc',
             body: {
-                "name": name,
-                "email": email,
-                "username": username,
-                "password": password,
-                "dateCreated": Date.now()
+                query: {
+                    bool: {
+                        filter: {
+                            term: {
+                                email: {
+                                    value: email
+                                }
+                            }
+                        }
+                    }
+                }
             }
-        }, function (err, resp) {
+        })
+        if (existsUser && existsUser.body.hits.hits.length > 0) {
+            res.status(400).send({ error: 'Tài khoản đã tồn tại' })
+        } else {
 
-            if (resp.body) {
-                // const result = resp.body.hits.hits;
-                res.send({ success: "Create user success" });
-                console.log(resp.body);
-                return;
-            } else if (err) {
-                err = err;
-                res.status(404).send({ error: err.name });
-                return;
-            }
-        });
+            bcrypt.hash(password, 10, async (err, hash) => {
+                if (err) {
+                    return res.status(500).send({ error: "Internal server error" });
+                }
+                else {
+                    let error;
+                    client.index({
+                        index: userIndex,
+                        type: '_doc',
+                        body: {
+                            "name": name,
+                            "email": email,
+                            "password": hash,
+                            "avatar": "https://www.iconpacks.net/icons/2/free-user-icon-3297-thumb.png",
+                            "dateCreated": Date.now()
+                        }
+                    }, function (err, resp) {
+
+                        if (resp.body) {
+                            // const result = resp.body.hits.hits;
+                            res.send({ success: "Create user success" });
+                            console.log(resp.body);
+                            return;
+                        } else if (err) {
+                            error = err;
+                            res.status(404).send({ error: error.name });
+                            return;
+                        }
+                    });
+                }
+            });
+
+
+        }
+
     } catch (error) {
         res.send(error);
-        // console.log(error)
+        console.log(error)
     }
 }
 
 const loginUser = async (req, res) => {
-    const { username, password } = req.body
-    console.log(username)
+    const { email, password } = req.body
+
     try {
         const result = await client.search({
             index: userIndex,
             body: {
                 query: {
                     bool: {
-                        must: [
-                            { match: { username: username } },
-                            { match: { password: password } },
-                        ]
+                        filter: {
+                            term: {
+                                email: {
+                                    value: email
+                                }
+                            }
+                        }
                     }
                 }
             }
         })
-        if (result.body.hits.total.value >= 1) {
-            // console.log(result.body.hits.hits)
-            const data = result.body.hits.hits[0];
-            const user = {
-                name: data._source.name,
-                id: data._id,
-                token: generateToken(data._id),
-            }
-            res.send({ success: "Đăng nhập thành công", user });
-            return;
+        const users = result.body.hits.hits;
+        if (users.length === 0) {
+            res.status(400).send({ error: "Tài khoản không tồn tại" })
         } else {
-            res.send({ error: "Sai tài khoản hoặc mật khẩu" })
+            console.log(users[0]._source.password)
+            bcrypt.compare(password, users[0]._source.password, async (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return res.status(500).send({ error: "Internal server error" });
+                }
+                if (!result) {
+                    return res.status(401).send({ error: "Sai mật khẩu" });
+                }
+                const user = {
+                    name: users[0]._source.name,
+                    id: users[0]._id,
+                    token: generateToken(users[0]._id),
+                }
+                res.status(200).send({ success: "Đăng nhập thành công", user });
+                return;
+            });
+
         }
     } catch (error) {
         console.log(error)
--- backend/routers/post.router.js
+++ backend/routers/post.router.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const postRouter = express.Router();
-import { newPost, getAllPost, getPostBySlug } from '../controllers/post.controller.js'
+import { newPost, getAllPost, getPostBySlug, searchPosts } from '../controllers/post.controller.js'
 
 postRouter.route('/:slug').get(getPostBySlug)
+postRouter.route('/search/:keyword').get(searchPosts);
 
 postRouter.route('/')
     .get(getAllPost)
